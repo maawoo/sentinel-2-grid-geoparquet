760b847d0ecaa06de2aa9e6c69a0b6e096081996
maawoo <marco.wolsza@uni-jena.de>
2026-01-16 15:29:34 +0100

    update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,10 @@
 import re
-import requests
-import zipfile
-from io import BytesIO
-
 import numpy as np
 import geopandas as gpd
 from geopandas.array import GeometryArray
 import pandas as pd
 import shapely
-from shapely import wkt, MultiPolygon, Point, Polygon
+from shapely import wkt, MultiPolygon, Point, Polygon, Geometry, STRtree
 
 URL_ESA_S2_GRID_KML = "https://sentiwiki.copernicus.eu/__attachments/1692737/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.zip"
 URL_NE_VEC_10m_LAND_GEOJSON = "https://github.com/nvkelso/natural-earth-vector/raw/v5.1.2/geojson/ne_10m_land.geojson"
@@ -28,7 +24,7 @@ def get_utm_wkt(row: pd.Series) -> str:
     utm_wkt : str
         UTM WKT string.
     """
-    text = row.Description.split('<b>')[-2]
+    text = row.description.split('<b>')[-2]
     m = re.findall(r'MULTIPOLYGON\(\(\((.+?)\)\)\)', text)
     utm_wkt = f"POLYGON (({m[0]}))"
     return utm_wkt
@@ -48,40 +44,56 @@ def get_epsg(row: pd.Series) -> int:
     epsg : int
         EPSG code.
     """
-    text = row.Description.split('<b>')[2]
+    text = row.description.split('<b>')[2]
     m = re.findall('<font COLOR="#008000">(.+?)</font>', text)
     epsg = int(m[0])
     return epsg
 
 
 def union_query_strtree(
-    tree: shapely.STRtree,
-    geometry: shapely.Geometry,
-    predicate: str = 'intersects',
-) -> np.ndarray:
-    """
+    gdf: gpd.GeoDataFrame,
+    gdf_to_query: gpd.GeoDataFrame,
+    predicate: str = 'intersects') -> np.ndarray:
+    """ 
     @thx sehoffmann
-    Queries a shapely STRtree with a geometry.
-    Unlike shapely.STRtree.query, this function performantely handles MultiPolygons by 
-    querying with their individual Polygons and then forming the union over the matching indices.
+    Perform a union query using shapely.STRtree spatial index. Unlike `shapely.STRtree.query`, 
+    this function efficiently handles MultiPolygons by querying with their individual 
+    Polygons and then forming the union over the matching indices.
 
     For some predicates, this can lead to different results than shapely.STRtree.query!
 
-    Args:
-        tree (shapely.STRtree): The spatial index to query.
-        geometry (shapely.Geometry): The geometry or list of geometries to query with.
-        predicate (str): The spatial predicate to use for querying. Defaults to 'intersects'.
-
-    Returns:
-        np.ndarray: Array of indices of geometries in the tree that match the predicate.
+    Parameters
+    ----------
+    gdf : geopandas.GeoDataFrame
+        GeoDataFrame containing geometries to be indexed.
+    gdf_to_query : geopandas.GeoDataFrame
+        GeoDataFrame containing geometries to query against the index.
+    predicate : str, optional
+        Spatial predicate to use for the query. Default is 'intersects'.
+    
+    Returns
+    -------
+    indices : numpy.ndarray
+        Array of indices of geometries in `gdf` that satisfy the predicate with any 
+        geometry in `gdf_to_query`.
     """
-    if isinstance(geometry, shapely.MultiPolygon):
+    # Ensure both GeoDataFrames are in the same CRS (WGS 84)
+    gdf = gdf.to_crs(epsg=4326)
+    gdf_to_query = gdf_to_query.to_crs(epsg=4326)
+
+    # Prepare the union geometry from gdf_to_query
+    geometry = shapely.union_all(gdf_to_query.geometry)
+    shapely.prepare(geometry)
+    if isinstance(geometry, MultiPolygon):
         geometries = geometry.geoms
-    elif isinstance(geometry, shapely.Geometry):
+    elif isinstance(geometry, Geometry):
         geometries = [geometry]
     else:
         raise ValueError('geometry must be a shapely.Geometry')
 
+    # Build STRtree from gdf geometries and perform union query
+    geoms = GeometryArray(np.array([g for g in gdf.geometry]))
+    tree = STRtree(geoms)
     all_indices = []
     for geometry in geometries:
         indices = tree.query(geometry, predicate=predicate)
@@ -93,60 +105,46 @@ def union_query_strtree(
         return np.unique(np.concatenate(all_indices))
 
 
-def handle_multipolygons_and_polygons(geom):
-    polys = [g for g in geom if not isinstance(g, Point)]
+def multipolygon_from_geoms(geoms: list[Geometry]) -> MultiPolygon | Polygon:
+    """ Construct a MultiPolygon or Polygon from a list of geometries """
+    polys = [g for g in geoms if not isinstance(g, Point)]
     if len(polys) == 1:
         return Polygon(polys[0])
     else:
         return MultiPolygon(polys)
 
 
+def center_from_geoms(geoms: list[Geometry]) -> Point:
+    """ Extract the center Point from a list of geometries """
+    return [g for g in geoms if isinstance(g, Point)][0]
+
+
 if __name__=='__main__':
-    # Load KML file directly from URL
-    url = URL_ESA_S2_GRID_KML
+    #################################################
+    # Load and process ESA Sentinel-2 grid KML file for all available tiles
+    gdf = gpd.read_file(URL_ESA_S2_GRID_KML, 
+                        engine="pyogrio", force_2d=True, 
+                        layer='Features',
+                        columns=["Name", "description", "geometry"])
+    gdf.rename(columns=dict(Name='tile'), inplace=True)
 
-    # download the zip
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    # read zip in memory
-    zf = zipfile.ZipFile(BytesIO(resp.content))
-    kml_name = next(name for name in zf.namelist() if name.lower().endswith(".kml"))
-    kml_bytes = zf.read(kml_name)
-
-    gdf = gpd.read_file(
-        BytesIO(kml_bytes),
-        engine="pyogrio",
-        force_2d=True,
-    )
-
-    gdf = gdf.rename(columns={"Name": "tile"})
-    
-    gdf['center'] = gdf.geometry.apply(lambda x: [g for g in x.geoms if isinstance(g, Point)][0])
-    gdf['geometry'] = gdf.geometry.apply(lambda x: handle_multipolygons_and_polygons(x.geoms))
-   
-    # Extract UTM_WKT and EPSG from "Description" column
+    gdf['center'] = gdf.geometry.apply(lambda x: center_from_geoms(x.geoms))
+    gdf['geometry'] = gdf.geometry.apply(lambda x: multipolygon_from_geoms(x.geoms))
     gdf['epsg'] = gdf.apply(get_epsg, axis=1)
     gdf['utm_wkt'] = gdf.apply(get_utm_wkt, axis=1)
+    gdf['utm_bounds'] = gdf.utm_wkt.apply(lambda x: wkt.loads(x).bounds).astype(str)
+    gdf = gdf.drop(columns=['description'])
 
-    # Add simple UTM bounds (left, down, right, up)
-    gdf[["utm_left", "utm_down", "utm_right", "utm_up"]] = (
-        gdf["utm_wkt"]
-        .apply(lambda x: wkt.loads(x).bounds)
-        .apply(pd.Series)
-    )
-
-    gdf = gdf.drop(columns=['Description'])
+    print(f"Saving GDF containing {len(gdf)} tiles to sentinel-2-grid.parquet...")
     gdf.to_parquet("sentinel-2-grid.parquet")
 
-    resp = requests.get(URL_NE_VEC_10m_LAND_GEOJSON)
-    resp.raise_for_status()
-    land_highres = gpd.read_file(BytesIO(resp.content), engine="pyogrio")
-    land_highres = land_highres.to_crs(epsg=4326)  # ensure WGS 84
-    land_highres = shapely.union_all(land_highres.geometry)
-    shapely.prepare(land_highres)
+    #################################################
+    # Filter tiles that intersect land using high resolution land mask
+    land_highres = gpd.read_file(URL_NE_VEC_10m_LAND_GEOJSON, engine="pyogrio")
 
-    geoms = GeometryArray(np.array([g for g in gdf.geometry]))
-    land = union_query_strtree(shapely.STRtree(geoms), land_highres)
-    gdf_land = gdf.iloc[land]
+    land = union_query_strtree(gdf=gdf, gdf_to_query=land_highres, 
+                               predicate='intersects')
+    gdf_land = gdf.iloc[land].reset_index(drop=True)
+
+    print(f"Saving GDF containing {len(gdf_land)} land tiles to sentinel-2-grid_LAND.parquet...")
     gdf_land.to_parquet("sentinel-2-grid_LAND.parquet")
